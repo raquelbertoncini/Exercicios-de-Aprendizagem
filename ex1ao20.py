9a1377d32b7fd56be63bcfec64bac010cb699e26
Raquel <raquel.doms@gmail.com>
2022-06-17 16:19:18 -0300

    Primeira parte dos exercícios 1-20

---
+++ ex1ao20.py
@@ -0,0 +1,31 @@
+'''print('1_ Faça um programa que leia um número inteiro e o imprima.')
+n = 10
+print(n)
+print('_ '*30)
+
+print('2_ Faça um programa que leia um número real e o imprima.')
+n = -15
+print(n)
+print('_ '*30)
+
+print('3_ Peça ao usuário para digitar 3 valores inteiros e imprima a soma deles.')
+n1 = int(input('Digite 1o número: '))
+n2 = int(input('Digite 2o número: '))
+n3 = int(input('Digite 3o número: '))
+soma = n1 + n2 + n3
+print(f'A soma dos 3 números é: {soma}.')
+print('_ '*30)
+
+print('4_ Leia um número real e imprima o resultado do quadrado desse número.')
+n = 20
+square = n * n
+print(square) 
+print('_ '*30)
+
+print('5_ Leia um número real e imprima a quinta parte deste número.')
+n = 25
+part5 = n /5
+print(part5) '''
+print('_ '*30)
+
+print('6_ Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit')
