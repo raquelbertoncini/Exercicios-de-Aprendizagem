6e5f0553b4ed1ba69f38bf1ca6bc874ecf55752b
Raquel <raquel.doms@gmail.com>
2022-06-23 09:06:30 -0300

    até ex 30

--- ex21ao40.py
+++ ex21ao40.py
@@ -1,1 +1,71 @@
+'''print('*'*30)
+print('21_ Leia um valor de massa em libras e apresente-o convertido em quilogramas.')
+libra = 160
+quilo = libra * 0.45
+print(f' {libra} libras são {quilo} kg.')
+print('*'*30)
 
+print('22_ Leia um valor de comprimento em jardas e apresente-o convertido em metros.')
+jardas = 800
+metro = 0.91 * jardas
+print(f'{jardas} jardas equilavem a {metro} metros.')
+print('*'*30)
+
+print('23_ Leia um valor de comprimento em metros e apresente-o convertido em jardas.')
+metro = 728
+jardas = metro / 0.91
+print(f'Tirando a prova... {metro} metros são {jardas} jardas.')
+print('*'*30)
+
+print('24_ Leia um valor de área em metros quadrados e apresente-o convertido em acres.')
+m2 = 400
+acre = m2 * 0.000247
+print(f'{m2} m2 = {acre} acres.')
+print('*'*30)
+
+print('25_ Leia um valor de área em acres e apresente-o convertido em metros quadrados')
+acre = 4
+m2 = acre * 4048.58
+print(f'{acre} acres são {m2} metros quadrados.')
+print('*'*30)
+
+print('26_ Leia um valor de área em metros quadrados e apresente-o convertido em hectares.')
+m2 = 3000
+hectare = m2 * 0.0001
+print(m2, 'metros quadrados')
+print(f'é o mesmo que {hectare} hectares.')
+print('*'*30)
+
+print('27_ Leia um valor de área em hectares e apresente-o convertido em metros quadrados')
+hect = 5
+metro = hect * 10000
+print(hect, 'hectares')
+print(metro, 'metros quadrados')
+print('*'*30)
+
+print('28_ Faça a leitura de 3 valores e apresente como resultado a soma dos quadrados dos 3 valores lidos.')
+v1 = 20
+v2 = 39
+v3 = 51
+resultado = (v1 ** 2) + (v2 ** 2) + (v3 ** 2)
+print (v1, v2, v3)
+print(f'A soma dos quadrados dos números apresentados é {resultado}.')
+print('*'*30)
+
+print('29_ Leia quatro notas, calcule a média aritmética e imprima o resultado.')
+n1 = 6
+n2 = 9
+n3 = 7
+n4 = 10
+media = (n1 + n2 + n3 + n4)/ 4
+print(n1, n2, n3, n4)
+print(f'Média aritmética = {media}.')
+print('*'*30)
+
+print('30_ Leia um valor em real e a cotação do dólar. Em seguida, imprima o valor correspondente em dólares.')
+real = 450
+cotacao = 5.19
+dolar = real * cotacao
+print(f' R$ {real} reais são {dolar:.2f} dólares no dia de hoje, 23 de junho de 2022.')
+print('*'*30)
+'''
