15b4a8a5582599dfe4a5201bd3a843791ecb3c7f
Raquel <raquel.doms@gmail.com>
2022-06-27 21:28:46 -0300

    ex 3

--- Udemy-Sessão5.py
+++ Udemy-Sessão5.py
@@ -1,1 +1,17 @@
+'''print('1_ Faça um programa que receba dois números e mostre qual deles é o maior.')
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+num = [n1, n2]
+print(max(num))
+print('* '* 30)
 
+import math
+print('2_ Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz quadrada, se for negativo, mostre uma mensagem dizendo que o número é inválido.')
+n = float(input('Digite um número: '))
+if n < 0:
+    print('Você não digitou um número válido!')
+elif n > 0:
+    n2 = math.sqrt(n)
+    print(f'A raiz quadrada de {n} é {n2:.2f}')
+'''
+print('3_ Leia um número real. Se o número for positivo imprima raiz quadrada. Do contrário, imprima o número ao quadrado.')
