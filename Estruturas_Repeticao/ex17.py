dc6835665187b93356311aea6491472253e5303a
Raquel <raquel.doms@gmail.com>
2022-09-01 14:14:01 -0300

    Adicionando exercícios -

--- Estruturas_Repeticao/ex17.py
+++ Estruturas_Repeticao/ex17.py
@@ -1,1 +1,14 @@
 print('Faça um programa que leia um número inteiro positivo N e calcule a soma dos N primeiros números naturais.')
+n = int(input('Digite um número: '))
+
+soma = 0
+
+if n > 0:
+    for i in range (0, n +1):
+        soma += i
+
+else:
+    print('Não é um número positivo')
+    
+
+print(soma)
