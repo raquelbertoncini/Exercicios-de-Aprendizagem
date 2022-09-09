8d71cc1b30c0037f96bf7a8a9b26be544e575d17
Raquel <raquel.doms@gmail.com>
2022-09-08 23:09:31 -0300

    Adicionando exercícios --

--- Estruturas_Repeticao/ex26.py
+++ Estruturas_Repeticao/ex26.py
@@ -1,3 +1,16 @@
 print('Faça um algoritmo que encontre o primeiro múltiplo de 11, 13 ou 17 após um número dado.')
 
-n = int(input('Digite o número de início: '))
+num = int(input('Entre com um número: '))
+while True:
+    if num % 11 == 0:
+        print(f'{num} é múltiplo de 11.')
+        break
+    elif num % 13 == 0:
+        print(f'{num} é múltiplo de 13.')
+        break
+    elif num % 17 == 0:
+        print(f'{num} é múltiplo de 17.')
+        break
+    num += 1
+
+''' Se quiséssemos achar um múltiplo de cada um dos números, retirar os 'breaks', somente deixar o úlitmo'''
---
+++ Estruturas_Repeticao/ex27.py
@@ -0,0 +1,6 @@
+print('Em Matemática, o número harmônico designado por H(n) define-se como sendo a soma da série harmônica:\n'
+'H(n) = 1 + 1/2 + 1/3 + 1/4 + ... + 1/n\n'
+'Faça um programa que leia um valor n inteiro e positivo e apresente o valor de H(n)')
+
+n = int(input('Digite um número: '))
+
